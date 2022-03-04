2836abf679b5980c8089284b19f321ffa0eb25b8
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-03-04 10:35:55 -0800

    link cascading WIP to CI
    link cascading WIP to CI

--- experimental-cascading/docker/link-cascading.sh
+++ experimental-cascading/docker/link-cascading.sh
@@ -3,8 +3,5 @@
 # drop cascading build back onto host machine
 # to make IDE resolution work for local development
 rm -rf /root/m2-host/repository/net/wensel/
+mkdir -p /root/m2-host/repository/net/wensel/
 cp -r /root/.m2/repository/net/wensel/ /root/m2-host/repository/net/wensel/
-
-# build scalding
-cd /scalding
-./sbt compile
