64b637b942bd36b780b2005cb42403290dda550e
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-03-04 10:58:05 -0800

    debug

--- experimental-cascading/docker/link-cascading.sh
+++ experimental-cascading/docker/link-cascading.sh
@@ -4,4 +4,5 @@
 # to make IDE resolution work for local development
 rm -rf /root/m2-host/repository/net/wensel/
 mkdir -p /root/m2-host/repository/net/wensel/
+ls /root/.m2/repository/net/wensel/
 cp -r /root/.m2/repository/net/wensel/ /root/m2-host/repository/net/wensel/
