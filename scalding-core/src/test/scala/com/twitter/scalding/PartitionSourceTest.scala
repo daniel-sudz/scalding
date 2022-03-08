d635daa79024d3cde1b92533504e4187484a3911
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-03-08 14:29:59 -0800

    more updates

--- scalding-core/src/test/scala/com/twitter/scalding/PartitionSourceTest.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/PartitionSourceTest.scala
@@ -136,7 +136,7 @@ class CustomPartitionSourceTest extends WordSpec with Matchers {
 
       val directory = new File(testMode.getWritePathFor(CustomPartitionedTsv))
 
-      directory.listFiles().map(_.getName()).toSet shouldBe Set("{A}->{x}", "{B}->{y}")
+      directory.listFiles().filter((fname) => !fname.getName().contains("SUCCESS")).map(_.getName()).toSet shouldBe Set("{A}->{x}", "{B}->{y}")
 
       val aSource = ScalaSource.fromFile(new File(directory, "{A}->{x}/part-00000-00000"))
       val bSource = ScalaSource.fromFile(new File(directory, "{B}->{y}/part-00000-00001"))
